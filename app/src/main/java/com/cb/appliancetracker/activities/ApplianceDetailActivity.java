e275352e681b8d119f1d9e2634c8344c0c3336e2
Amrish Sharma <amrishsharma95@gmail.com>
2025-04-25 16:41:01 +0530

    updated code

--- app/src/main/java/com/cb/appliancetracker/activities/ApplianceDetailActivity.java
+++ app/src/main/java/com/cb/appliancetracker/activities/ApplianceDetailActivity.java
@@ -60,5 +60,7 @@ public class ApplianceDetailActivity extends AppCompatActivity {
                 }
             });
         }).start();
+
+
     }
 }
--- app/src/main/java/com/cb/appliancetracker/activities/ApplianceListActivity.java
+++ app/src/main/java/com/cb/appliancetracker/activities/ApplianceListActivity.java
@@ -2,8 +2,8 @@ package com.cb.appliancetracker.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.widget.Button;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +17,7 @@ import java.util.List;
 
 public class ApplianceListActivity extends AppCompatActivity {
 
+    private Button addButton;
     private RecyclerView recyclerView;
     private ApplianceAdapter applianceAdapter;
 
@@ -25,9 +26,20 @@ public class ApplianceListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appliance_list);
 
+        addButton = findViewById(R.id.add_button);
+
         recyclerView = findViewById(R.id.recyclerViewAppliances);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Set up the onClickListener for the Add button
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the ApplianceDetailActivity to add a new appliance
+                Intent intent = new Intent(ApplianceListActivity.this, ApplianceDetailActivity.class);
+                startActivity(intent);
+            }
+        });
         // Fetch appliance data in a background thread
         new Thread(() -> {
             List<Appliance> appliances = ApplianceDatabase.getInstance(this).applianceDao().getAllAppliances();
